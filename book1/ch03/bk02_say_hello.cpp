b78775a0e035a27177e0155ee20c29a35ff574f9
ed <>
2021-11-22 14:59:33 +0000

    Add book1/ch03
    FossilOrigin-Name: e4fd53abd4189765822375f9dfa6ecdb5e074a94eb182a206efb26f6b71ad9f7

---
+++ book1/ch03/bk02_say_hello.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello, I am your computer talking." << endl;
+    cout << 5 + 10 << endl;
+
+    return 0;
+}
+
+// Hello, I am your computer talking.
+// 15
